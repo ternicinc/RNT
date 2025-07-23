2fe57038e560a79a1b2e8a00dd108cccaf1a6b4b
ternicinc <allexanderlinux@gmail.com>
2025-07-23 11:46:18 +0200

    Intial Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const fs = require("fs");
+
+// Custom Imports
+// const ipManager = require("ipManager.js");
+
+function main() {
+    try {
+        console.log('Hello, World.');
+    }
+    catch {
+        error("ERROR")
+    }
+}
+
+main();
